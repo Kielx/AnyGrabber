55afb65056d40c26d5f19176d702116ffaf3f696
Krzysztof Pantak <kielx.dev@gmail.com>
2023-02-27 22:56:12 +0100

    Update fetching function, add log info to textbox

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-from utils.file_operations import get_appdata_logs, get_programdata_logs
+from utils.file_operations import get_anydesk_logs
+from os import getenv
 
 import customtkinter
+
+app_data_path = getenv('APPDATA')
+app_data_filename = f'{app_data_path}/AnyDesk/ad.trace'
+program_data_path = getenv('PROGRAMDATA')
+program_data_filename = f'{program_data_path}/AnyDesk/ad_svc.trace'
 
 
 class App(customtkinter.CTk):
@@ -8,7 +14,7 @@ class App(customtkinter.CTk):
         super().__init__()
 
         self.geometry("500x300")
-        self.title("small example app")
+        self.title("AnyGrabber")
         self.minsize(300, 200)
 
         # create 2x2 grid system
@@ -16,19 +22,25 @@ class App(customtkinter.CTk):
         self.grid_columnconfigure((0, 1), weight=1)
 
         self.textbox = customtkinter.CTkTextbox(master=self)
+
         self.textbox.grid(row=0, column=0, columnspan=2, padx=20, pady=(20, 0), sticky="nsew")
 
         self.button = customtkinter.CTkButton(master=self, command=self.button_callback, text="Fetch logs")
         self.button.grid(row=1, column=0, columnspan=2, padx=20, pady=20, sticky="ew")
 
     def button_callback(self):
-        self.textbox.insert("insert", "Fetching programdata logs: \n")
-        self.textbox.insert("insert", get_programdata_logs() + "\n\n")
+        self.textbox.delete("0.0", "end")  # delete all text
+        log_entries = get_anydesk_logs(app_data_filename)
         self.textbox.insert("insert", "Fetching appdata logs: \n")
-        self.textbox.insert("insert", get_appdata_logs() + "\n")
+        for entry in log_entries:
+            self.textbox.insert("insert", entry + "\n\n")
+        log_entries = get_anydesk_logs(program_data_filename)
+        self.textbox.insert("insert", "Fetching programdata logs: \n")
+        for entry in log_entries:
+            self.textbox.insert("insert", entry + "\n\n")
+        self.textbox.configure(state="disabled")
 
 
 if __name__ == "__main__":
     app = App()
     app.mainloop()
-
