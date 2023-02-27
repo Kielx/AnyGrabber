08ff0dbc540c7a7f85de0cbb1c81fabb787a556b
Krzysztof Pantak <kielx.dev@gmail.com>
2023-02-27 22:56:12 +0100

    Update fetching function, add log info to textbox

--- utils/file_operations.py
+++ utils/file_operations.py
@@ -1,43 +1,21 @@
-from os import getenv
+import dateutil.parser as dparser
 
-app_data_path = getenv('APPDATA')
-app_data_filename = f'{app_data_path}/AnyDesk/ad.trace'
-program_data_path = getenv('PROGRAMDATA')
-program_data_filename = f'{program_data_path}/AnyDesk/ad_svc.trace'
 search_string = 'Logged in from '
 
 
-def get_appdata_logs():
+def get_anydesk_logs(filepath: str):
     try:
-        with open(app_data_filename, 'r') as f:
+        with open(filepath, 'r') as f:
+            log_entries = []
             for l_no, line in enumerate(f):
                 # search string
                 if search_string in line:
-                    print('string found in a file')
-                    print('Line Number:', l_no)
-                    print('Line:', line)
                     before_keyword, keyword, after_keyword = line.partition(search_string)
                     last_dot = after_keyword.rfind(' on relay')
-                    print(last_dot)
                     after_keyword = after_keyword[0: last_dot]
-                    print(after_keyword)
+                    date_of_login = dparser.parse(before_keyword[0:30], fuzzy=True)
+                    date_of_login = date_of_login.strftime("%d/%m/%Y, %H:%M:%S")
+                    log_entries.append(date_of_login + "  -  " + after_keyword)
+        return log_entries
     except IOError:
-        return f'Could not open file {app_data_filename}'
-
-
-def get_programdata_logs():
-    try:
-        with open(program_data_filename, 'r') as f:
-            for l_no, line in enumerate(f):
-                # search string
-                if search_string in line:
-                    print('string found in a file')
-                    print('Line Number:', l_no)
-                    print('Line:', line)
-                    before_keyword, keyword, after_keyword = line.partition(search_string)
-                    last_dot = after_keyword.rfind(' on relay')
-                    print(last_dot)
-                    after_keyword = after_keyword[0: last_dot]
-                    print(after_keyword)
-    except IOError:
-        return f'Could not open file {program_data_filename}'
+        return f'Could not open file {filepath}'
