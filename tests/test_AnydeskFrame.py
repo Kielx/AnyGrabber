27b9f068ea369a3bd5458c7dee7972f9619394e2
Kielx <kielx.dev@gmail.com>
2023-03-31 14:54:27 +0200

    Add test if open report button shows after search

--- tests/test_AnydeskFrame.py
+++ tests/test_AnydeskFrame.py
@@ -1,3 +1,5 @@
+import os
+
 from frames.AnydeskFrame import AnydeskFrame
 
 
@@ -34,3 +36,16 @@ def test_turn_off_switches():
                                      anydesk_frame.fetch_programdata_logs_switch, anydesk_frame.find_files_switch])
 
     assert anydesk_frame.fetch_appdata_logs_switch.get() is False
+
+
+def test_if_open_report_button_shows_after_search():
+    # Arrange
+    anydesk_frame = AnydeskFrame(master=None)
+
+    assert anydesk_frame.open_report_button.grid_info() == {}
+    anydesk_frame.search_filesystem_callback(search_location=os.getcwd())
+    assert anydesk_frame.open_report_button.grid_info() != {}
+    assert anydesk_frame.open_report_button.grid_info()["in"] == anydesk_frame
+    os.remove('report.txt')
+
+
