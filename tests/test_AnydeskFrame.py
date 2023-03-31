3753f766c4fdebe7f1ebfe31cf40b4cae819b835
Krzysztof Pantak <kielx.dev@gmail.com>
2023-03-31 18:38:34 +0200

    Cleanup and refactor AnydeskFrame.py

--- tests/test_AnydeskFrame.py
+++ tests/test_AnydeskFrame.py
@@ -6,31 +6,32 @@ def test_turn_off_switches():
     anydesk_frame = AnydeskFrame(master=None)
 
     # Test turning off switches independently
-    anydesk_frame.find_files_switch.set(True)
-    anydesk_frame.turn_off_switches([anydesk_frame.find_files_switch])
-    assert anydesk_frame.find_files_switch.get() is False
+    anydesk_frame.switch_search_for_logs_in_location.set(True)
+    anydesk_frame.turn_off_switches([anydesk_frame.switch_search_for_logs_in_location])
+    assert anydesk_frame.switch_search_for_logs_in_location.get() is False
 
-    anydesk_frame.fetch_programdata_logs_switch.set(True)
-    anydesk_frame.turn_off_switches([anydesk_frame.fetch_programdata_logs_switch])
-    assert anydesk_frame.fetch_programdata_logs_switch.get() is False
+    anydesk_frame.switch_fetch_programdata_logs.set(True)
+    anydesk_frame.turn_off_switches([anydesk_frame.switch_fetch_programdata_logs])
+    assert anydesk_frame.switch_fetch_programdata_logs.get() is False
 
-    anydesk_frame.fetch_appdata_logs_switch.set(True)
-    anydesk_frame.turn_off_switches([anydesk_frame.fetch_appdata_logs_switch])
-    assert anydesk_frame.fetch_appdata_logs_switch.get() is False
+    anydesk_frame.switch_fetch_appdata_logs.set(True)
+    anydesk_frame.turn_off_switches([anydesk_frame.switch_fetch_appdata_logs])
+    assert anydesk_frame.switch_fetch_appdata_logs.get() is False
 
-    anydesk_frame.fetch_appdata_logs_switch.set(True)
-    anydesk_frame.fetch_programdata_logs_switch.set(True)
-    anydesk_frame.turn_off_switches([anydesk_frame.fetch_appdata_logs_switch,
-                                     anydesk_frame.fetch_programdata_logs_switch])
+    anydesk_frame.switch_fetch_appdata_logs.set(True)
+    anydesk_frame.switch_fetch_programdata_logs.set(True)
+    anydesk_frame.turn_off_switches([anydesk_frame.switch_fetch_appdata_logs,
+                                     anydesk_frame.switch_fetch_programdata_logs])
 
-    assert anydesk_frame.fetch_appdata_logs_switch.get() is False
-    assert anydesk_frame.fetch_programdata_logs_switch.get() is False
+    assert anydesk_frame.switch_fetch_appdata_logs.get() is False
+    assert anydesk_frame.switch_fetch_programdata_logs.get() is False
 
-    anydesk_frame.fetch_appdata_logs_switch.set(True)
-    anydesk_frame.fetch_programdata_logs_switch.set(True)
-    anydesk_frame.find_files_switch.set(True)
+    anydesk_frame.switch_fetch_appdata_logs.set(True)
+    anydesk_frame.switch_fetch_programdata_logs.set(True)
+    anydesk_frame.switch_search_for_logs_in_location.set(True)
 
-    anydesk_frame.turn_off_switches([anydesk_frame.fetch_appdata_logs_switch,
-                                     anydesk_frame.fetch_programdata_logs_switch, anydesk_frame.find_files_switch])
+    anydesk_frame.turn_off_switches([anydesk_frame.switch_fetch_appdata_logs,
+                                     anydesk_frame.switch_fetch_programdata_logs,
+                                     anydesk_frame.switch_search_for_logs_in_location])
 
-    assert anydesk_frame.fetch_appdata_logs_switch.get() is False
+    assert anydesk_frame.switch_fetch_appdata_logs.get() is False
