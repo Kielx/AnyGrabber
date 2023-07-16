de4d9dcab69b91a199c9310d5533e84d0c91e675
Krzysztof Pantak <kielx.dev@gmail.com>
2023-07-16 22:59:17 +0200

    Base for moving to event based searching for anydesk logs

---
+++ utils/event_utils.py
@@ -0,0 +1,13 @@
+# https://stackoverflow.com/questions/58925648/registering-a-function-to-be-called-on-an-event-in-python
+class myEvent:
+    def __init__(self):
+        self.callbacks = list()
+
+    def registerCallback(self, callback):
+        self.callbacks.append(callback)
+
+    def call(self):
+        print('This event has fired')
+
+        for callback in self.callbacks:
+            callback()
